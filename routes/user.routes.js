f1f4a31c03d317018db18e864bf9c714fdf12c17
Ricardo Vazquez <rickyrabv@gmail.com>
2024-01-14 00:06:07 +0100

    changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,10 +7,17 @@ const Set = require("../models/Set.model")
 const Part = require("../models/Part.model")
 
 router.get("/profile", isLoggedIn, (req, res, next) => {
-    res.render("user/profile", {userInSession : req.session.currentUser })
+    console.log(req.session.currentUser);
+    User.findById(req.session.currentUser._id).populate('favoriteBuilds')
+    .then(foundUser => {
+        let sets = foundUser.favoriteBuilds; 
+        return res.render("user/profile", { userInSession: req.session.currentUser, mySets: sets })
+    })
+    .catch(err => next(err));
+    
 })
 router.get("/edit-profile", isLoggedIn, (req, res, next) => {
-    
+
 })
 
 module.exports = router;
--- seeds/parts.seed.js
+++ seeds/parts.seed.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const mongoose = require('mongoose');
 const Part = require("../models/Part.model")
 axios
-    .get(`https://rebrickable.com/api/v3/lego/sets/11002-1/parts/?key=${process.env.REBRICK_KEY}`)
+    .get(`https://rebrickable.com/api/v3/lego/sets/11001-1/parts/?key=7412840ee8015bc8eed2f0b3dde0a1ec`)
     .then(response => {
         console.log('Response from API: ', response.data.results);
         let resultsArray = response.data.results
