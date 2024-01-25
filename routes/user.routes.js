4060dd459db68aafb3446b2ba02522c9475ca3ff
Ricardo Vazquez <rickyrabv@gmail.com>
2024-01-25 20:32:59 +0100

    routes refactored partially, UD for User added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,30 +3,68 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 const router = express.Router();
 const User = require("../models/User.model")
 const Set = require("../models/Set.model")
-const Part = require("../models/Part.model")
+const Part = require("../models/Part.model");
+const fileUploader = require('../config/cloudinary');
 
-router.get("/profile", isLoggedIn, (req, res, next) => {
-    Set.find().populate('creators')
-    .then(foundSets => {
-        console.log(foundSets);
-        console.log(req.session.currentUser.username);
-        const userSets = foundSets.filter(set => set.creators.username === req.session.currentUser.username)
-        return res.render("user/profile", { userInSession: req.session.currentUser, mySets: userSets })
-    })
-    .catch(err => next(err));
-    
-})
-router.get("/edit-profile/:id", isLoggedIn, (req, res, next) => {
-    const { id } = req.params; 
-    User.findById(id)
-    .then(user => {
-        return res.render("user/edit-user", {userInSession: req.session.currentUser})
-    })
-    .catch(err => next(err));
+router.get("/profile", isLoggedIn, async (req, res, next) => {
+    try {
+    let user = await User.findById(req.session.currentUser._id)
+    let allSets = await Set.find().populate('creators')
+    let userSets = await allSets.filter(set => set.creators.username === user.username)
+    return res.render("user/profile", { userInSession: user, mySets: userSets })
+    } 
+    catch(err) { 
+    next(err)
+    }  
 })
 
-router.post("/edit-profile/:id", (req, res, next) => {
-    
+router.get("/edit/:id", isLoggedIn, async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        let user = await User.findById(id)
+        return res.render("user/edit-user", {userInSession: user})
+    } 
+    catch (error) {
+        next(err)
+    }
+})
+
+router.post("/edit/:id", fileUploader.single('profilePicture'), async (req, res, next) => {
+    try {
+    const { id } = req.params;
+    console.log(req.body);
+    const { existingImage } = req.body; 
+    let profilePicture; 
+    if (req.file) {
+        profilePicture = req.file.path;
+    } else {
+        profilePicture = existingImage
+    }
+    let user = await User.findByIdAndUpdate(id, { profilePicture }, { new: true })
+    console.log(user);
+    return res.redirect("/user/profile")
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+router.post("/delete/:id", async (req, res, next) => {
+    try {
+        console.log("req.params", req.params);
+        const { id } = req.params;
+        let user = await User.findByIdAndDelete(id);
+        let logout = await req.session.destroy(err => {
+            if (err) {
+                res.status(500).render("auth/logout", { errorMessage: err.message });
+                return;
+            }
+        })
+        return res.redirect("/")
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router;
